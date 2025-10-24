c3b50ac041c56e1ba80277bbec1959fde4889d04
Linat20 <agress2000@gmail.com>
2025-10-24 10:13:03 +0500

    добавлена фоновая картинка

---
+++ public/splash.js
@@ -0,0 +1,14 @@
+let progress = 0;
+const progressBar = document.querySelector('.progress');
+
+const interval = setInterval(() => {
+  progress += 2; // шаг
+  progressBar.style.width = progress + '%';
+
+  if (progress >= 100) {
+    clearInterval(interval);
+    setTimeout(() => {
+      window.location.href = 'menu.html';
+    }, 500);
+  }
+}, 60); // длительность ~3 сек
